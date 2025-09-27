637b832abf52b55ca5e4394553ea50b74117ffc8
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-09-27 22:24:53 +0300

    feat(book view/create): add form view

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,45 @@
+<x-layout :title="'Create an Book'">
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="bg-neutral-800/30 p-8 rounded-md shadow-lg shadow-black/50">
+                <form>
+                    <div class="space-y-12">
+                        <div class="border-b border-white/10 pb-12">
+                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="sm:col-span-3">
+                                    <label for="title" class="block text-sm/6 font-medium text-white">Title</label>
+                                    <div class="mt-2">
+                                        <input id="title" type="text" name="title" autocomplete="title" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-700 sm:text-sm/6" />
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-3">
+                                    <label for="author" class="block text-sm/6 font-medium text-white">Author</label>
+                                    <div class="mt-2">
+                                        <input id="author" type="text" name="author" autocomplete="author" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-700 sm:text-sm/6" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="col-span-full">
+                                    <label for="description" class="block text-sm/6 font-medium text-white">Description</label>
+                                    <div class="mt-2">
+                                        <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-700 sm:text-sm/6"></textarea>
+                                    </div>
+                                    <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about the book.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-6 flex items-center justify-end gap-x-6">
+                        <a href="{{ route('books.index') }}" type="button" class="text-sm/6 font-semibold text-white hover:text-white/60">Cancel</a>
+                        <button type="submit" class="rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700 hover:bg-green-500">Save</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </main>
+</x-layout>
