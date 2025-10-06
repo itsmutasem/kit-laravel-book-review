576d5b945556d1ed2bcdcfe16346a5bbe9ef99ce
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-06 06:54:55 -0700

    Day 10: use uuid

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasUlids;
+    // UUID
+    public $incrementing = false;
+    protected $keyType = 'string';
+    
     //table
     protected $table = 'books';
 
